4fa6daec0b81470d3a68db583eae3776dc5195b3
Thiago Batista <thiago.batista41@etec.sp.gov.br>
2025-11-11 18:56:08 -0300

    alteracoes

--- Program.cs
+++ Program.cs
@@ -14,8 +14,12 @@ namespace GestaoAluno
             Menu display = new Menu();
             Aluno aluno = new Aluno();
             MySqlConnection conexao;
+            bool continuar = true;
 
-            conexao = new MySqlConnection("server=127.0.0.1;uid=root;pwd='1234';database=escola; Port=3307");
+
+            while (continuar)
+            {
+                conexao = new MySqlConnection("server=127.0.0.1;uid=root;pwd='1234';database=escola; Port=3307");
 
             try
             {
@@ -25,23 +29,28 @@ namespace GestaoAluno
             {
                 Console.WriteLine("Erro ao conectar ao banco de dados. \n" + erro.Message);
             }
-
+ 
             switch (display.Hud())
             {
                 case "1":
+                    Console.Clear();
                     aluno.Cadastro();
-
+                    Console.Clear();
                     string sqlInsert = "INSERT INTO alunos (Nome, Idade, Curso) VALUES (@nome, @idade, @curso)";
                     MySqlCommand cmdCA = new MySqlCommand(sqlInsert, conexao);
                     cmdCA.Parameters.AddWithValue("@nome", aluno.Nome);
                     cmdCA.Parameters.AddWithValue("@idade", aluno.Idade);
                     cmdCA.Parameters.AddWithValue("@curso", aluno.Curso);
                     int linhas = cmdCA.ExecuteNonQuery();
-                    Console.WriteLine("Aluno Cadastrado com sucesso.");
+                    Console.WriteLine("Aluno Cadastrado com sucesso. \nPressione ENTER para voltar ao menu...");
+                    Console.ReadLine();
+                    Console.Clear();
+
                     break;
 
                 case "2":
-                    string sqlSelect = "SELECT Id, Nome, Idade, Curso FROM alunos";
+                    Console.Clear();
+                     string sqlSelect = "SELECT Id, Nome, Idade, Curso FROM alunos";
                      MySqlCommand cmdLI = new MySqlCommand(sqlSelect, conexao);
                      MySqlDataReader reader = cmdLI.ExecuteReader();
                     while (reader.Read())
@@ -52,15 +61,20 @@ namespace GestaoAluno
                         string curso = reader.GetString("Curso");
                         Console.WriteLine($"ID: {id} Nome: {nome} Idade: {idade} Curso: {curso}");
                     }
-                    break;
+                        Console.WriteLine("\nPressione ENTER para voltar ao menu...");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
 
                 case "3":
+                    Console.Clear();
                     aluno.Buscar();
-
+                    Console.Clear();
                     string sqlSelect1 = "SELECT Id, Nome, Idade, Curso FROM alunos WHERE Nome LIKE @busca";
                     MySqlCommand cmdBU = new MySqlCommand(sqlSelect1, conexao);
                     cmdBU.Parameters.AddWithValue("@busca", aluno.NomeBusca);
                     MySqlDataReader reader1 = cmdBU.ExecuteReader();
+                    Console.Clear();
 
                     while (reader1.Read())
                     {
@@ -70,7 +84,40 @@ namespace GestaoAluno
                         string curso = reader1.GetString("Curso");
                         Console.WriteLine($"ID: {id} Nome: {nome} Idade: {idade} Curso: {curso}");
                     }
+                        Console.WriteLine("\nPressione ENTER para voltar ao menu...");
+                        Console.ReadLine();
+                        Console.Clear();
                     break;
+
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine("Ainda não funciona...");
+                        continuar = false;
+                        break;
+
+                    case "5":
+                        Console.Clear();
+                        Console.WriteLine("Ainda não funciona...");
+                        continuar = false;
+                        break;
+
+                    case "6":
+                        Console.Clear();
+                        Console.WriteLine("Ainda não funciona...");
+                        continuar = false;
+                        break;
+
+                    case "0":
+                        Console.Clear();
+                        Console.WriteLine("Fechando Programa...");
+                        continuar = false;
+                        break;
+
+                    default: 
+                        Console.WriteLine("opção inválida");
+                        continuar = false;
+                        break;
+            }
             }
         }
         }
